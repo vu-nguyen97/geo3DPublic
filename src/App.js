e967d82df9407822fd1d730e0fe07ed7540db9ff
Nguyen Minh Duc (VTI.D2) <duc.nguyenminh@vti.com.vn>
2020-11-18 10:03:54 +0700

    remove double click event

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.scss';
-import { Viewer, Entity, EntityDescription, EntityStaticDescription, Camera, CameraFlyTo } from "resium";
-import { Cartesian3, Rectangle } from "cesium";
+import { Viewer, Entity } from "resium";
+import { Cartesian3, ScreenSpaceEventType } from "cesium";
 import { PureComponent } from 'react'
 import { hot } from "react-hot-loader/root";
 import {
@@ -242,6 +242,7 @@ class App extends PureComponent {
     this.viewer.scene.screenSpaceCameraController.minimumZoomDistance = 70000;
     this.viewer.scene.screenSpaceCameraController.maximumZoomDistance = 40000000;
     this.viewer.scene.screenSpaceCameraController._minimumZoomRate = 30000;
+    this.viewer.screenSpaceEventHandler.removeInputAction(ScreenSpaceEventType.LEFT_DOUBLE_CLICK)
 
     this.viewer.camera.changed.addEventListener(() => this.onZoom());
   }
@@ -409,7 +410,7 @@ class App extends PureComponent {
 
     let entitiesRender = null
 
-    if (showCities && !showProjectEntities) {
+    if (showCityEntities && !showProjectEntities) {
       entitiesRender = this.generateEntities(cities, true)
     } else {
       entitiesRender = this.generateEntities(entities)
